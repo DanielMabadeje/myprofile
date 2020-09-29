80107824f3740896e8cf1cf95a6d84a6f8a7b44c
Daniel <mabadejedaniel1@gmail.com>
2020-09-29 07:47:36 -0700

    edited loader

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,14 +1,8 @@
 var myVar;
-// effect1();
-// effect2();
 function myFunction() {
-    myVar = setTimeout(showPage, 7000);
+    myVar = setTimeout(showPage, 3000);
 }
 
 function showPage() {
   document.getElementById("loader").style.display = "none";
-  // document.getElementById("loader").style.height = "0";
-  // document.getElementById("content").style.display = "block";
-//   document.getElementById("logo").style.display = "initial";
-  // document.getElementById("logo").style.position = "relative";
 }
